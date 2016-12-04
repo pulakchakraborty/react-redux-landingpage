d7a899aa3a3949d9a22ce070158df8a0841e6dec
sanjaymenon <sanjayzzzz@gmail.com>
2016-12-05 03:54:09 +0530

    Moving assets reference

--- dev/js/reducers/reducer-landingpage.js
+++ dev/js/reducers/reducer-landingpage.js
@@ -2,7 +2,7 @@ export default function(){
 
 	var landingPageData = {
 		topcontent:{
-			image :"../../../assets/images/background.jpg",
+			image :"/assets/images/background.jpg",
 			heading:"Simply your best accountant",
 			caption:"best person match via just some questions",
 			buttontext:"GET YOUR MATCH"
@@ -13,17 +13,17 @@ export default function(){
 		},
 		rowtwo:{
 			colone:{
-				icon:"../../../assets/images/icon-profile.png",
+				icon:"/assets/images/icon-profile.png",
 				heading:"Personal and Relevant",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar. Maecenas ut aliquet risus, ut auctor purus."
 			},
 			coltwo:{
-				icon:"../../../assets/images/icon-profile.png",
+				icon:"/assets/images/icon-profile.png",
 				heading:"Matching Smartly",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar. Maecenas ut aliquet risus, ut auctor purus."
 			},
 			colthree:{
-				icon:"../../../assets/images/icon-coffee.png",
+				icon:"/assets/images/icon-coffee.png",
 				heading:"Collaboration",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar. Maecenas ut aliquet risus, ut auctor purus."
 			}
@@ -33,17 +33,17 @@ export default function(){
 			data:"Morbi non metus metus. Praesent vestibulum, lacus et ultricies cursus, mi lectus mollis nisi, vel porttitor libero turpis non urna. Proin tempor ipsum massa, in varius risus porttitor ut. Cras ac nisl id turpis bibendum efficitur a laoreet tellus. Suspendisse ornare gravida felis ut gravida."
 		},
 		rowfour:{
-				icon:"../../../assets/images/feature-section-dont-waste-time-en.png",
+				icon:"/assets/images/feature-section-dont-waste-time-en.png",
 				heading:"Dont waste time with a broad search",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar."
 		},
 		rowfive:{
-				icon:"../../../assets/images/feature-section-personal-makes-your-difference-en.png",
+				icon:"/assets/images/feature-section-personal-makes-your-difference-en.png",
 				heading:"Personal makes your difference",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar."
 		},
 		rowsix:{
-				icon:"../../../assets/images/feature-section-direct-contact-en.png",
+				icon:"/assets/images/feature-section-direct-contact-en.png",
 				heading:"Direct Contact",
 				data:"Nullam augue ligula, tincidunt sed ultrices eget, consectetur at lacus. Curabitur sodales ultricies pulvinar."
 		}
